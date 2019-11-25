6351ef4d786be0cd7a0651141735c0c81516d91f
jokessenich <johnny.kessenich@gmail.com>
2019-11-25 10:32:31 -0800

    added options

--- src/app.js
+++ src/app.js
@@ -21,9 +21,27 @@ app.use(cors({
     origin: CLIENT_ORIGIN
 }))
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    //intercepts OPTIONS method
+    if ('OPTIONS' === req.method) {
+      //respond with 200
+      res.send(200);
+    }
+    else {
+    //move on
+      next();
+    }
+});
+
 app.get('/', (req, res)=> {
     res.send('Hello, world!')
 })
+
+
 
 app.use('/api/diapers', DiapersRouter)
 app.use('/api/nursing', NursingRouter)
